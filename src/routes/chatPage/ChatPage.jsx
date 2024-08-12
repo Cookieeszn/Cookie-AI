84330323aa8fcc8f5f14cc508c1a1d263c074a6e
cookieeszn <klauscity01@gmail.com>
2024-08-12 00:34:08 -0700

    Added content

---
+++ src/routes/chatPage/ChatPage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const chatPage = () => {
+  return (
+    <div className="chatPage">
+      <div className="wrapper">
+        <div className="chat">
+          <div className="message">Test message</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default chatPage;
